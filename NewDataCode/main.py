f1647f5d06d4bb999c21b26e87faf38250c3ee4c
Murali T S <mt788@cornell.edu>
2020-05-04 15:25:20 -0400

    Update

--- NewDataCode/main.py
+++ NewDataCode/main.py
@@ -16,7 +16,7 @@ from MonteCarlo import MonteCarlo
 Building model to predict the acceptance/denial
 
 """
-
+"""
 orig_stdout = sys.stdout
 f = open('OriginalModelResults7030.txt', 'w')
 sys.stdout = f
@@ -65,10 +65,11 @@ tune_model(X,y,name = 'acceptance_denial_tuned',n_it = 50, models = ['RandomFore
 #################################################################################################
 
 """
+"""
 The above lines of code have been commented out because the models built have been saved.
 
 Predict if acceptance/denial and filter that to predict denial reason
-
+"""
 """
 
 filename = 'acceptance_denial_tuned.sav'
@@ -93,31 +94,31 @@ plt.savefig("denial_reason_prediction_histogram.png")
 sys.stdout = orig_stdout
 f.close()
 """
-
+"""
 Studying Bias and Fairness in the data with the next 100000 datapoints in 2017
 
 """
 
-# filename = 'acceptance_denial_tuned.sav'
-# accden = pickle.load(open(filename, 'rb'))
-# ColName = 'action_taken'
-# data = get_data(ColName,nr = 100000,year = 2017,sk = 100000)
+filename = 'acceptance_denial_tuned.sav'
+accden = pickle.load(open(filename, 'rb'))
+ColName = 'action_taken'
+data = get_data(ColName,nr = 100000,year = 2017,sk = 100000)
 # data.to_csv('new_test_data.csv')
-# original = np.array(data[ColName])
-# X = data.drop(columns = [ColName])
-# need = accden.predict(X)
+original = np.array(data[ColName])
+X = data.drop(columns = [ColName])
+need = accden.predict(X)
+X['accden'] = need
+X1  = X[X['accden'] == 3]
+X1 = X1.drop(columns = ['accden'])
+filename = 'denial_reason_tuned.sav'
+denreason = pickle.load(open(filename, 'rb'))
+reason = denreason.predict(X1)
 # X['accden'] = need
-# X1  = X[X['accden'] == 3]
-# X1 = X1.drop(columns = ['accden'])
-# filename = 'denial_reason_tuned.sav'
-# denreason = pickle.load(open(filename, 'rb'))
-# reason = denreason.predict(X1)
-# # X['accden'] = need
-# X = X.drop(columns = ['accden'])
-# # print(np.sum(original == need))
-# # print(np.sum(original == need) * (1/len(original)) ) #Accuracy is Roughly 52%
-# ori = X['applicant_sex']
-# old_pred = need
+X = X.drop(columns = ['accden'])
+# print(np.sum(original == need))
+# print(np.sum(original == need) * (1/len(original)) ) #Accuracy is Roughly 52%
+ori = X['applicant_sex']
+old_pred = need
 
 #########################
 
@@ -165,27 +166,29 @@ original = np.array(data[ColName])
 X = data.drop(columns = [ColName])
 total = X[(X['applicant_sex'] == 1) | (X['applicant_sex'] == 2)].shape[0]
 X = X[X['applicant_sex'] == 1]
+ori = X['applicant_sex']
 need = accden.predict(X)
 X['applicant_sex'] = [2 for j in range(X.shape[0])]
 print("Ratio of males in the data =",X.shape[0]/total)
 monte = accden.predict(X)
 X['new_pred'] = monte
-X['original_sex'] = ori
+X['original_sex'] = [1 for i in range(X.shape[0])]
 X['old_pred'] = need
-# female_to_male_accept = X[(X['applicant_sex'] == 1) & (X['original_sex'] == 2) & (X['new_pred'] == 1) & (X['old_pred'] == 3)].shape[0]
+print(np.sum(need == monte))
+female_to_male_accept = X[(X['applicant_sex'] == 1) & (X['original_sex'] == 2) & (X['new_pred'] == 1) & (X['old_pred'] == 3)].shape[0]
 male_to_female_accept = X[(X['applicant_sex'] == 2) & (X['original_sex'] == 1) & (X['new_pred'] == 1) & (X['old_pred'] == 3)].shape[0]
 male_to_female_reject = X[(X['applicant_sex'] == 2) & (X['original_sex'] == 1) & (X['new_pred'] == 3) & (X['old_pred'] == 1)].shape[0]
-# female_to_male_reject = X[(X['applicant_sex'] == 1) & (X['original_sex'] == 2) & (X['new_pred'] == 3) & (X['old_pred'] == 1)].shape[0]
+female_to_male_reject = X[(X['applicant_sex'] == 1) & (X['original_sex'] == 2) & (X['new_pred'] == 3) & (X['old_pred'] == 1)].shape[0]
 
 # total_female_to_male = X[(X['applicant_sex'] == 1) & (X['original_sex'] == 2)].shape[0]
 # total_male_to_female = X[(X['applicant_sex'] == 2) & (X['original_sex'] == 1)].shape[0]
 
 # no_change = X[(X['applicant_sex'] ==  X['original_sex'])].shape[0]
 
-# print(female_to_male_accept)
+print(female_to_male_accept)
 print(male_to_female_accept)
 print(male_to_female_reject)
-# print(female_to_male_reject)
+print(female_to_male_reject)
 
 male_to_female_accept_prob =(male_to_female_accept/(male_to_female_accept + male_to_female_reject))
 male_to_female_reject_prob =(male_to_female_reject/(male_to_female_accept + male_to_female_reject))
@@ -204,22 +207,23 @@ original = np.array(data[ColName])
 X = data.drop(columns = [ColName])
 total = X[(X['applicant_sex'] == 1) | (X['applicant_sex'] == 2)].shape[0]
 X = X[X['applicant_sex'] == 2]
+ori = X['applicant_sex']
 need = accden.predict(X)
 X['applicant_sex'] = [1 for j in range(X.shape[0])]
 print("Ratio of females in the data =",X.shape[0]/total)
 
 monte = accden.predict(X)
 X['new_pred'] = monte
-X['original_sex'] = ori
+X['original_sex'] = [2 for i in range(X.shape[0])]
 X['old_pred'] = need
 female_to_male_accept = X[(X['applicant_sex'] == 1) & (X['original_sex'] == 2) & (X['new_pred'] == 1) & (X['old_pred'] == 3)].shape[0]
-# male_to_female_accept = X[(X['applicant_sex'] == 2) & (X['original_sex'] == 1) & (X['new_pred'] == 1)].shape[0]
-# male_to_female_reject = X[(X['applicant_sex'] == 2) & (X['original_sex'] == 1) & (X['new_pred'] == 3)].shape[0]
+# male_to_female_accept = X[(X['applicant_sex'] == 2) & (X['original_sex'] == 1) & (X['new_pred'] == 1)& (X['old_pred'] == 3)].shape[0]
+# male_to_female_reject = X[(X['applicant_sex'] == 2) & (X['original_sex'] == 1) & (X['new_pred'] == 3)& (X['old_pred'] == 3)].shape[0]
 female_to_male_reject = X[(X['applicant_sex'] == 1) & (X['original_sex'] == 2) & (X['new_pred'] == 3) & (X['old_pred'] == 1)].shape[0]
 
 print(female_to_male_accept)
-# print(male_to_female_accept)
-# print(male_to_female_reject)
+print(male_to_female_accept)
+print(male_to_female_reject)
 print(female_to_male_reject)
 
 female_to_male_accept_prob =(female_to_male_accept/(female_to_male_accept + female_to_male_reject))
