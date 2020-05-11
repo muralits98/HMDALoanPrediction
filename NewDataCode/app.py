69df2a64478b0c64f7d29bcd8b536a0c5d78c09d
Murali T S <mt788@cornell.edu>
2020-05-11 13:16:44 -0400

    FINAL UPDATE

--- NewDataCode/app.py
+++ NewDataCode/app.py
@@ -26,23 +26,63 @@ app.layout = html.Div([
         style={'width': '25%',
                'display': 'inline-block'}),
     dcc.Graph(id='funnel-graph',figure = {}),
+    html.Div(
+        [
+            dcc.Dropdown(
+                id="plotting",
+                options=[{
+                    'label': i,
+                    'value': i
+                } for i in action_options],
+                value='loan_purpose'),
+        ],
+        style={'width': '25%',
+               'display': 'inline-block'}),
+    dcc.Graph(id = 'scattervalue',figure = {})
 ])
 
 @app.callback(
-    dash.dependencies.Output('funnel-graph', 'figure'),
+    dash.dependencies.Output('funnel-graph', 'figure')
     [dash.dependencies.Input('action', 'value')])
 
-def update_graph(action):
+def update_graph(action,plotting):
     df = data.groupby(action).count()['action_taken']
-    trace1 = go.Bar(x = [i for i in range(1,np.size(df.unique())+1)],y = df)
-    return {
-    'data': [trace1],
+    a = {
+    'data': [go.Bar(x = [i for i in range(1,np.size(df.unique())+1)],y = df)],
     'layout':
     go.Layout(
         title='Showing the count of number of occurences of values of column {} in the dataset'.format(action),
         barmode='stack')
     }
+    # b =  {
+    # 'data': [go.scatter(x = data[plotting],y = data[action_taken])],
+    # 'layout':
+    # go.Layout(
+    #     title='Showing the count of number of occurences of values of column {} in the dataset'.format(action),
+    #     barmode='stack')
+    # }
+    return a
+@app.callback(
+    dash.dependencies.Output('scattervalue', 'figure')
+    [dash.dependencies.Input('plotting', 'value')])
 
+def new_graph(action,plotting):
+    df = data.groupby(action).count()['action_taken']
+    # a = {
+    # 'data': [go.Bar(x = [i for i in range(1,np.size(df.unique())+1)],y = df)],
+    # 'layout':
+    # go.Layout(
+    #     title='Showing the count of number of occurences of values of column {} in the dataset'.format(action),
+    #     barmode='stack')
+    # }
+    b =  {
+    'data': [go.scatter(x = data[plotting],y = data[action_taken])],
+    'layout':
+    go.Layout(
+        title='Showing the count of number of occurences of values of column {} in the dataset'.format(action),
+        barmode='stack')
+    }
+    return b
 if __name__ == "__main__":
     ColName = 'action_taken'
     data = get_data(ColName,nr = 100000)
